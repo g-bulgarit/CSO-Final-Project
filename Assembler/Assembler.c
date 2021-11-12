371596d44bdd220f1c2b1c9a9451fe9acd2777af
g-bulgarit <alonio6@gmail.com>
2021-11-12 13:11:27 +0200

    [Assembler] Parser: Now supports (and requires) file input from command line
    Closes #4

--- Assembler/Assembler.c
+++ Assembler/Assembler.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 #include "Parser.h"
 
-int main() {
+int main(int argc, char *argv[]) {
 	// Open the file,
 	// On first pass, hold the addresses of all labels and return
 	// On second pass, replace labels with corresponding addresses, and write resulting bit-lines to file.
-	int retval = ParseFile( /*Put file path here*/ );
+	
+	if (argc != 2) {
+		printf("ASM file must be supplied...");
+		exit(1);
+	}
+	else {
+		char* InputFilePath = argv[1];
+		int retval = ParseFile(InputFilePath);
+	}
 }
