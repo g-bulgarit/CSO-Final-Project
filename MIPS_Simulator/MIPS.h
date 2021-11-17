405473ea2202826b6ef497ee6a33355e03f1cb6b
itay8zilka <itayzilka@gmail.com>
2021-11-17 21:28:48 +0200

    [MIPS] added basic data structure and 'add' function
    close #6

---
+++ MIPS_Simulator/MIPS.h
@@ -0,0 +1,11 @@
+//
+// Created by Itay Zilka on 17/11/2021.
+//
+
+#ifndef MIPS_SIMULATOR_MIPS_H
+#define MIPS_SIMULATOR_MIPS_H
+
+void add(int* mips, int rd, int rs, int rt, int rm);
+
+
+#endif //MIPS_SIMULATOR_MIPS_H
--- MIPS_Simulator/MIPS_Simulator.c
+++ MIPS_Simulator/MIPS_Simulator.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "MIPS.h"
 
 int main() {
-	printf("fuck visual studio");
+	int mips_p[16] = {0};
+
+	mips_p[11] = 7;
+	mips_p[12] = 3;
+	add(mips_p, 10, 11, 12, 0);
+	printf("%d", mips_p[10]);
 }
