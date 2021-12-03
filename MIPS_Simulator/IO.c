8735662f4d773415fd776483d4b4249074f9eff3
g-bulgarit <alonio6@gmail.com>
2021-12-03 10:56:14 +0200

    [MIPS] Added skeleton functions for disk IO

---
+++ MIPS_Simulator/IO.c
@@ -0,0 +1,14 @@
+#include "IO.h"
+int hardDrive[128][16] = { 0 };
+
+
+void ReadDiskFromFile(int* cycle) {
+
+};
+
+void WriteDiskToFile(int* cycle) {
+
+};
+
+void ReadSector() {};
+void WriteSector() {};
--- MIPS_Simulator/IO.h
+++ MIPS_Simulator/IO.h
@@ -1,1 +1,10 @@
 #pragma once
+#include "Constants.h"
+unsigned int hw_reg[HW_REGISTER_AMOUNT] = { 0 };
+
+#define DISK_CYCLE_USAGE 1024;
+
+#define DISKCMD 14;
+#define DISKSECTOR 15;
+#define DISKBUFFER 16;
+#define DISKSTATUS 17;
