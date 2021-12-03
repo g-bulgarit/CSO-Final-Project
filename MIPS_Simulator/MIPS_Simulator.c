67e4d41d9297a50d6aa037ef95c2b5b5c23cbb6f
g-bulgarit <alonio6@gmail.com>
2021-12-03 10:35:31 +0200

    [MIPS] Removed testing function for mips operations

--- MIPS_Simulator/MIPS_Simulator.c
+++ MIPS_Simulator/MIPS_Simulator.c
@@ -3,52 +3,6 @@
 #include "Constants.h"
 #include "MIPS.h"
 
-void testing(int argc, char* argv[]) {
-	char* hexInstructionsFilePath = argv[1];
-	char* memoryFilePath = argv[2]; // currently unused.
-
-	// Define array to hold all MIPS registers.
-	int mips_test[16] = { 0 };
-	// Some tests...
-	mips_test[0] = 7; // 00..0000111
-	mips_test[1] = 3; // 00..0000011
-	mips_test[2] = 1; // 00..0000001
-	mips_test[3] = 1; // 00..0000001
-	add(mips_test, 0, 1, 2, 3);
-	printf("add:  %d\n", mips_test[0] == 5);
-	sub(mips_test, 0, 1, 2, 3);
-	printf("sub:  %d\n", mips_test[0] == 1);
-	mac(mips_test, 0, 1, 2, 3);
-	printf("mac:  %d\n", mips_test[0] == 4);
-	and (mips_test, 0, 1, 2, 3);
-	printf("and1: %d\n", mips_test[0] == 1);
-	or (mips_test, 0, 1, 2, 3);
-	printf("or1:  %d\n", mips_test[0] == 3);
-	xor (mips_test, 0, 1, 2, 3);
-	printf("xor1: %d\n", mips_test[0] == 3);
-
-	mips_test[1] = 7;  // 00..0000111
-	mips_test[2] = 5;  // 00..0000101
-	mips_test[3] = 13; // 00..0001101
-	and (mips_test, 0, 1, 2, 3);
-	printf("and2: %d\n", mips_test[0] == 5);
-	or (mips_test, 0, 1, 2, 3);
-	printf("or2:  %d\n", mips_test[0] == 15);
-	xor (mips_test, 0, 1, 2, 3);
-	printf("xor2: %d\n", mips_test[0] == 15);
-
-	mips_test[1] = 1;  // 00..0000001
-	mips_test[2] = 2;  // 00..0000010
-	mips_test[3] = 4;  // 00..0000100
-	and (mips_test, 0, 1, 2, 3);
-	printf("and3: %d\n", mips_test[0] == 0);
-	or (mips_test, 0, 1, 2, 3);
-	printf("or3:  %d\n", mips_test[0] == 7);
-	xor (mips_test, 0, 1, 2, 3);
-	printf("xor3: %d\n", mips_test[0] == 7);
-
-	registerTrace(mips_test, 124, "1400201420132");
-}
 
 int partOfStringToInt(char* string,int start, int length) {
 	// Function to split string to substrings and convert these substrings to integers.
