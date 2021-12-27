4c5afec860422ef5b2a97007009abd6c94a6e78e
g-bulgarit <alonio6@gmail.com>
2021-12-27 21:53:19 +0200

    [ASM] Draw a circle :)
    Closes #32

--- MIPS_Simulator/Assembly/circle/circle.asm
+++ MIPS_Simulator/Assembly/circle/circle.asm
@@ -4,10 +4,11 @@
 # OPCODE RD     RS     RT     RM IMM1 IMM2
     sll $sp, $imm1, $imm2, $zero, 1, 11		    # set $sp = 2048
     out $zero, $imm1, $zero, $imm2, 21, 255     # Set all pixels full white
-    lw $s0, $imm1, $zero, $zero, 0x100, 0       # S0 = Circle diameter
-    sra $s1, $s0, $imm1, $zero, 1, 0            # S1 = Circle radius
-    sub $s2, $imm1, $s1, $zero, 128, 0          # Get min offset
-    mac $s0, $s1, $imm1, $s2, 2, 0              # Get max offset
+    lw $s1, $imm1, $zero, $zero, 0x100, 0       # S0 = Circle diameter
+    # sra $s1, $s0, $imm1, $zero, 1, 0            # S1 = Circle radius
+    sub $s2, $imm1, $s1, $zero, 128, 0          # S2 =  min offset
+    mac $s0, $s1, $imm1, $s2, 2, 0              # S0 =  max offset
+    mac $s1, $s1, $s1, $zero, 0, 0              # S1 = rad^2
 
     # t0 = rows, t1 = cols
     add $t0, $imm1, $s2, $zero, -1, 0           # t0 = min_offset - 1
@@ -27,13 +28,24 @@ rows:
 
 cols:
     add $t1, $imm1, $t1, $zero, 1, 0           # t1 += 1
-    blt $zero, $t1, $s0, $imm2, 0, logic   # check if t1 < max_offset
+    blt $zero, $t1, $s0, $imm2, 0, logic       # check if t1 < max_offset
     beq $zero, $zero, $zero, $imm2, 0, rows    # else
 
 logic:
+    sub $t0, $t0, $imm1, $zero, 128, 0         # t0 cooridinate shift
+    sub $t1, $t1, $imm1, $zero, 128, 0         # t1 cooridinate shift
+    mac $t2, $t0, $t0, $zero, 0, 0             # t2 = row^2
+    mac $t2, $t1, $t1, $t2, 0, 0               # t2 = (x-128)^2 + (y-128)^2
+    jal $ra, $zero, $zero, $imm2, 0, recover 
     mac $a0, $t0, $imm1, $t1, 256, 0           # a0 = monitor offset (rows*256 + cols)
+    bgt $zero, $t2, $s1, $imm2, 0, cols     # if t2 > r^2, no draw!
     jal $ra, $zero, $zero, $imm2, 0, draw
     beq $zero, $zero, $zero, $imm2, 0, cols
+
+recover:
+    add $t0, $t0, $imm1, $zero, 128, 0         # t0 cooridinate shift
+    add $t1, $t1, $imm1, $zero, 128, 0         # t1 cooridinate shift
+    beq $zero, $zero, $zero, $ra, 0, 0
 
 endloop:
     halt $zero, $zero, $zero, $zero, 0, 0
