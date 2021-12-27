1bf4265b9688bec1d0b41133733f587658231102
g-bulgarit <alonio6@gmail.com>
2021-12-27 20:35:03 +0200

    [ASM] Circle: now draws centered(!) rectangle

--- MIPS_Simulator/Assembly/circle/circle.asm
+++ MIPS_Simulator/Assembly/circle/circle.asm
@@ -4,11 +4,14 @@
 # OPCODE RD     RS     RT     RM IMM1 IMM2
     sll $sp, $imm1, $imm2, $zero, 1, 11		    # set $sp = 2048
     out $zero, $imm1, $zero, $imm2, 21, 255     # Set all pixels full white
-    lw $s0, $imm1, $zero, $zero, 0x100, 0       # S0 = CIRCLE DIAMETER
+    lw $s0, $imm1, $zero, $zero, 0x100, 0       # S0 = Circle diameter
+    sra $s1, $s0, $imm1, $zero, 1, 0            # S1 = Circle radius
+    sub $s2, $imm1, $s1, $zero, 128, 0          # Get min offset
+    mac $s0, $s1, $imm1, $s2, 2, 0              # Get max offset
 
     # t0 = rows, t1 = cols
-    add $t0, $imm1, $zero, $zero, -1, 0          # t0 = -1
-    beq $zero, $zero, $zero, $imm2, 0, rows      # Jump to rows
+    add $t0, $imm1, $s2, $zero, -1, 0           # t0 = min_offset - 1
+    beq $zero, $zero, $zero, $imm2, 0, rows     # Jump to rows
 
 draw:
     # literally draw:
@@ -18,13 +21,13 @@ draw:
 
 rows:
     add $t0, $imm1, $t0, $zero, 1, 0           # t0 += 1
-    add $t1, $imm1, $zero, $zero, -1, 0        # t1 = -1
-    blt $zero, $t0, $imm1, $imm2, 256, cols    # check if t0 < 256
+    add $t1, $imm1, $s2, $zero, -1, 0          # t1 = min_offset - 1
+    blt $zero, $t0, $s0, $imm2, 0, cols        # check if t0 < max_offset
     beq $zero, $zero, $zero, $imm2, 0, endloop # else
 
 cols:
     add $t1, $imm1, $t1, $zero, 1, 0           # t1 += 1
-    blt $zero, $t1, $imm1, $imm2, 256, logic   # check if t1 < 256
+    blt $zero, $t1, $s0, $imm2, 0, logic   # check if t1 < max_offset
     beq $zero, $zero, $zero, $imm2, 0, rows    # else
 
 logic:
