563ca597bc245f6e393e3062f14dfc25731e4789
g-bulgarit <alonio6@gmail.com>
2022-01-01 20:13:24 +0200

    [ASM] Disktest: Finished disktest program

--- MIPS_Simulator/Assembly/disktest/disktest.asm
+++ MIPS_Simulator/Assembly/disktest/disktest.asm
@@ -1,63 +1,27 @@
-# 128 ints per sector (128 words) = 512bytes
-# $s2 = copySectorIdx
-# $t2 = diskstatus
-    add $s2, $zero, $zero, $imm1, 6, 0   # s2 = 7 (copySectorIdx = 6)
+    add $s2, $zero, $zero, $imm1, 7, 0              # $s2 = 7 (Start sector is 7, will be moved to 8)
 
-loop1:
-    blt $zero, $s2, $imm, $imm2, 0, end  # check if we are done
+start:
+    blt $zero, $s2, $imm1, $imm2, 0, end            # If the sector number is <0 we're done, go to halt
 
 check_ready_1:
-    in $t2, $imm1, $zero, $zero, 17, 0  # 1 if busy
-    beq $zero, $t2, $imm1, $imm2, 1, check_ready_1 # disk busy
+    in $t2, $imm1, $zero, $zero, 17, 0              # Check if disk is busy
+    beq $zero, $t2, $imm1, $imm2, 1, check_ready_1  # If busy, go up one line
     
-    out $zero, $imm1, $zero, $s2, 15, 0  # Set disk sector = $s2
-    out $zero, $imm1, $zero, $zero, 16, 0  # Set mem addr = $zero
-    out $zero, $imm1, $zero, $imm2, 14, 1  # Set diskcmd = read
+    out $zero, $imm1, $zero, $s2, 15, 0             # Set disk sector = $s2 for reading
+    out $zero, $imm1, $zero, $zero, 16, 0           # Set mem addr = $zero
+    out $zero, $imm1, $zero, $imm2, 14, 1           # Set diskcmd = read
 
 check_ready_2:
-    in $t2, $imm1, $zero, $zero, 17, 0  # 1 if busy
-    beq $zero, $t2, $imm1, $imm2, 1, check_ready_2 # disk busy
+    in $t2, $imm1, $zero, $zero, 17, 0              # Check if disk is busy
+    beq $zero, $t2, $imm1, $imm2, 1, check_ready_2  # If busy, go up one line
 
-    add $s2, $zero, $zero, $imm1, 1, 0  # increment idx
-    out $zero, $imm1, $zero, $s2, 15, 0  # Set disk sector = $s2
-    out $zero, $imm1, $zero, $zero, 16, 0  # Set mem addr = $zero
-    out $zero, $imm1, $zero, $imm2, 14, 2  # Set diskcmd = read
+    add $s2, $s2, $zero, $imm1, 1, 0                # increment idx by one for write target
+    out $zero, $imm1, $zero, $s2, 15, 0             # Set disk sector = $s2 for writing
+    out $zero, $imm1, $zero, $zero, 16, 0           # Set mem addr = $zero
+    out $zero, $imm1, $zero, $imm2, 14, 2           # Set diskcmd = write
 
-    sub $s2, $zero, $zero, $imm1, 2, 0  # increment idx by 2
-    bne $zero, $zero, $zero, $imm2, 0, loop1
+    sub $s2, $s2, $imm1, $zero, 2, 0                # Decrement idx by 2 to continue with program flow
+    beq $zero, $zero, $zero, $imm2, 0, start        # Jump to start, unconditionally
 
 end:
-    halt $zero, $zero, $zero, $zero, 0, 0
-# Initialization:
-# CopySectorIdx = 6
-
-# copy_sector():
-#     A: while diskcmd != 0:
-#         goto A
-#   
-#     do stuff
-#
-#     B: while diskcmd != 0:
-#         goto B
-#   
-#     do stuff
-#
-#
-
-# while (CopySectorIdx >= 0):
-#     copy_sector()
-#     CopySectorIdx -= 1
-
-
-
-# copy-sector function():
-#   Check disk status (while true)
-#   READ:
-#       set disk sector to copy FROM
-#       set ram[0] to be the buffer for the data
-#       read sector into ram (diskcmd = 1)
-#
-#   Check disk status (while true)
-#   WRITE:
-#       set disksector to copy TO
-#       diskcmd = 1
+    halt $zero, $zero, $zero, $zero, 0, 0           # End program. Observe the beautiful diskout file. 
--- MIPS_Simulator/Assembly/disktest/generate_sectors.py
+++ MIPS_Simulator/Assembly/disktest/generate_sectors.py
@@ -1,11 +1,11 @@
-sector_data = ["0", "1", "2", "3", "4", "5", "6", "7"]
-sectors = 512
-words_in_sector = 4
+sector_data = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+bytes_in_sector = 512
+bytes_in_word = 4
 
 with open("diskin.txt", "w+") as wfp:
     lines = []
     for dat in sector_data:
         val = "0000000" + dat + "\n"
-        for _ in range((sectors // words_in_sector)):
+        for _ in range((bytes_in_sector // bytes_in_word)):
             wfp.write(val)
             
